a5078c392a3eb8a46488c631036af89e4a8df1ae
Asid <sidoniefernandes1@gmail.com>
2022-09-25 12:24:24 +0200

    Added change password features

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -4,12 +4,15 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\UserFormType;
+use App\Form\ChangePasswordFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
+#[Route('/account')]
 class AccountController extends AbstractController
 {
 
@@ -21,13 +24,13 @@ class AccountController extends AbstractController
     }
 
     
-    #[Route('/account', name: 'app_account', methods: 'GET')]
+    #[Route('', name: 'app_account', methods: 'GET')]
     public function show(): Response
     {
         return $this->render('account/show.html.twig');
     }
 
-    #[Route('/account/edit', name: 'app_account_edit', methods: 'GET|POST')]
+    #[Route('/edit', name: 'app_account_edit', methods: 'GET|POST')]
     public function edit(Request $request): Response
     {   
         $user = $this->getUser();
@@ -42,17 +45,52 @@ class AccountController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             
-            $this->em->persist($user);
             $this->em->flush();
-            
 
-            $this->addFlash('success', 'Profil successfully updated !');
+            $this->addFlash('success', 'Account successfully updated !');
 
-            return $this->redirectToRoute('app_home');
+            return $this->redirectToRoute('app_account');
             
         }
         return $this->render('account/edit.html.twig', [
             'userForm' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/change-password', name: 'app_account_change', methods: 'GET|POST')]
+    public function change(Request $request, UserPasswordHasherInterface $passwordHasher): Response
+    {   
+        if(!$this->getUser()) {
+            $this->addFlash('warning', 'You cannot change password');
+
+            return $this->redirectToRoute('app_login');
+        }
+        
+        $user = $this->getUser();
+        //dd($user);
+        $form = $this->createForm(ChangePasswordFormType::class, null, [
+            'current_password_is_required' => true
+        ]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // encode the plain password
+            $user->setPassword(
+                $passwordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+            
+            $this->em->flush();
+
+            $this->addFlash('success', 'Password successfully updated !');
+            return $this->redirectToRoute('app_account');
+            
+        }
+        return $this->render('account/change.html.twig', [
+            'resetPasswordForm' => $form->createView(),
         ]);
     }
 }
--- src/Form/UserFormType.php
+++ src/Form/UserFormType.php
@@ -5,16 +5,33 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 
 class UserFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstName')
-            ->add('lastName')
-            ->add('email')
+        ->add('firstName', TextType::class, [
+            'attr' => [
+                'class' => 'form-control'
+            ],
+        ])
+        ->add('lastName', TextType::class, [
+            'attr' => [ 
+                'class' => 'form-control'
+            ]
+        ])
+        ->add('email', EmailType::class, [
+            'attr' => [
+                'class' => 'form-control'
+            ],
+            'label' => 'e-mail'
+        ])
         ;
     }
 
